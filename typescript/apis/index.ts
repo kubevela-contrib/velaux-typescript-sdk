d9eab06153d8ab550006e0926766c6e8212a40fb
Z Zhang <zuoning@AndrewZs-MacBook-Air.local>
2023-07-26 23:56:35 -0700

    typescript SDK files

---
+++ typescript/apis/index.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddonApi';
+export * from './AddonRegistryApi';
+export * from './ApplicationApi';
+export * from './AuthenticationApi';
+export * from './CloudshellApi';
+export * from './ClusterApi';
+export * from './ConfigApi';
+export * from './DefinitionApi';
+export * from './EnvApi';
+export * from './HelmApi';
+export * from './OamApplicationApi';
+export * from './PayloadTypesApi';
+export * from './PipelineApi';
+export * from './PluginApi';
+export * from './ProjectApi';
+export * from './RbacApi';
+export * from './RepositoryApi';
+export * from './SystemInfoApi';
+export * from './TargetApi';
+export * from './UsersApi';
+export * from './VelaQLApi';
+export * from './WebhookApi';
